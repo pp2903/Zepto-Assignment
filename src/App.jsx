ab017c78c7dc09c360d386723d39f0447326fe14
pp2903 <priyampranshu@gmail.com>
2024-01-16 15:48:02 +0530

    UI fix and responsiveness added

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,20 @@
 import './App.css'
 import SearchBar from './components/SearchBar'
 import Chip from './components/Chip'
+import { user_data } from './data/userData'
+import { createContext } from 'react'
+export const AppContext = createContext({})
 function App() {
-  
-  
+  const people = user_data  
+
   return (
-    <div className='grid grid-cols-1 gap-1 place-content-center '>
+
+    <AppContext.Provider value={people}>
+    <div className='app-container grid grid-cols-1 gap-1 self-center'>
     <SearchBar></SearchBar>
     {/* <Chip></Chip> */}
     </div>
+    </AppContext.Provider>
   )
 }
 
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,16 +1,51 @@
 // import { useRef,useState,useReducer } from "react";
+import { useContext } from "react";
 import Chip from "./Chip";
 import Dropdown from "./Dropdown";
+import { AppContext } from "../App";
 const SearchBar = () => {
+
+
+
+  const data = useContext(AppContext)
+  console.log("Printing from searchBar component");
+  console.log(data)
+  const handleInputSelect = (e) => {
+    // console.log(e)
+    console.log("Input element clicked");
+  };
+
+  const handleBlur = () => {
+    console.log("Clicked away!");
+  };
+
+
+
+
+
   return (
     <>
-      <div className="search-bar-container bg-white rounded-full  min-w-max flex mb-0 pb-0">
+      <div className="search-bar-container bg-white rounded-full flex flex-wrap max-w-[200px] md:max-w-md  mb-0 pb-0">
         <Chip></Chip>
+        <Chip></Chip>
+        <Chip></Chip>
+        <Chip></Chip>
+        <Chip></Chip>
+        <Chip></Chip>
+        <Chip></Chip>
+        <Chip></Chip>
+        <Chip></Chip>
+        <Chip></Chip>
+        <Chip></Chip>
+        <Chip></Chip>
+
         <input
           className="rounded-lg m-2 ml-0 p-2 h-3 w-64  focus:outline-none "
           type="text"
-          placeholder="Search..."
-          value=""
+          placeholder="Add new user..."
+          
+          onFocus={handleInputSelect}
+          onBlur={handleBlur}
         />
       </div>
       <Dropdown> </Dropdown>
